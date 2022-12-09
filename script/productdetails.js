eb5c7f691610468615cf741119954ce13555b4b2
Richard Perez <richard.floresperez0@gmail.com>
2022-12-09 10:08:42 -0500

    Added the image to display corresponding to the category it represents based on the product

--- script/productdetails.js
+++ script/productdetails.js
@@ -1,11 +1,14 @@
 "use strict";
+const forImagesArray = ["drinks", "condiments", "confections", "dairyproducts", "grains", "meats", "produce", "seafood"];
+const image = document.getElementById("categoryPicture");
+
 window.onload = function () {
     const urlParams = new URLSearchParams(location.search);
     const row = document.getElementById("showAllProductData");
     let id = -1;
     if (urlParams.has("productid")) {
         id = urlParams.get("productid");
-        if (id < 1 ) window.location.href = "productsSearch.html";
+        if (id < 1) window.location.href = "productsSearch.html";
         else {
             fetch(`http://localhost:8081/api/products/${id}`)
                 .then((response) => response.json())
@@ -14,10 +17,15 @@ window.onload = function () {
                         let cell = row.insertCell(-1);
                         cell.innerHTML = data[x];
                     }
+
+                    image.src = `./images/${forImagesArray[data["categoryId"] - 1]}.jpg`;
+                    image.style.display = "block";
                 });
+
         }
     }
     else {
         window.location.href = "productsSearch.html";
     }
+
 }
--- script/productsSearch.js
+++ script/productsSearch.js
@@ -1,56 +1,49 @@
 "use strict";
+
 const mainDropdown = document.getElementById("mainProductSearch");
 const tableBody = document.getElementById("displayProductData");
-let categoriesArray = [];
+const categoryDropdown = document.getElementById("byCategoryDropdown");
+const forImagesArray = ["drinks", "condiments", "confections", "dairyproducts", "grains", "meats", "produce", "seafood"];
+const table = document.querySelector("table");
+const image = document.getElementById("categoryPicture");
 window.onload = function () {
-    const categoryDropdown = document.getElementById("byCategoryDropdown");
-    const table = document.querySelector("table");
+
     //using an arrow function inside an anonymous function with both being event handlers for different "things"; and using the shorthand syntax for the if stament
     mainDropdown.onchange = () => {
-        mainDropdown.value === "byCategory" ? byCategoriesDropdown(categoryDropdown, table) : displayAllProducts(categoryDropdown, table);
+        if (mainDropdown.value === "byCategory") byCategoriesDropdown();
+        else if (mainDropdown.value === "allProducts") displayAllProducts();
     };
+    categoryDropdown.onchange = categoryDropdownOnChange;
 }
 
-function byCategoriesDropdown(categoryDropdown, table) {
+function byCategoriesDropdown() {
     categoryDropdown.style.display = "block";
     table.style.display = "";
-    categoryDropdown.selectedIndex = 0;
+    categoryDropdown.selectedIndex = 0; //make sure the table is always displaying the "Select one" option first
     categoryDropdown.length = 1;  //prevent the table from repeating to add the same categories  
-
-    //once the data is back, store it all in this array so I don't have to make another call to it
 
     fetch("http://localhost:8081/api/categories").then(response => response.json())
         .then(data => {
             for (let x of data) {
                 let option = new Option(x.name, x.name.toLowerCase());
-                categoryDropdown.appendChild(option);
-                categoriesArray.push(x);
+                categoryDropdown.appendChild(option)
             }
         });
-    /* once the second dropdown has been changed, meaning that an option was selected, get the value of the dropdown and 
-        call the function to display products based on that value(which is the name propert lowercased)
-    */
 
-    categoryDropdown.onchange = () => {
-        table.style.display = "block";
-        let rows = document.querySelectorAll("tbody tr");
-        Array.from(rows).forEach(row => tableBody.removeChild(row));
-        //find the object that was selected so that we can call the function that will make the fetch call to display data based on the argument passed(a category)
-        const findCategory = categoriesArray.find(category => category.name.toLowerCase() === categoryDropdown.value);
-        displayProductPerCategory(findCategory);
-    };
 }
 //using the numOfCategories, we're just making a call to every
-function displayAllProducts(categoryDropdown, table) {
-
+function displayAllProducts() {
+    image.style.display = "";
+    //first delete any rows in the table, show the table and hide the category dropdown
     let rows = document.querySelectorAll("tbody tr");
     Array.from(rows).forEach(row => tableBody.removeChild(row));
     categoryDropdown.style.display = "";
     table.style.display = "block";
 
+    //make the fetch call to get ALL the products
     fetch("http://localhost:8081/api/products").then(response => response.json()).then(products => {
-        let productsArray = products.sort( (x, y) => x.productName < y.productName ? -1 : x.productName === y.productName ? 0: 1);
-
+        //store that ENTIRE array into productsArray, sort that list by the name properties and use that instead to add them to rows
+        let productsArray = products.sort((x, y) => x.productName < y.productName ? -1 : x.productName === y.productName ? 0 : 1);
         for (let aProduct of productsArray) {
             let row = tableBody.insertRow(-1);
             //each object(element) of the array, loop through the properties and display only 3 by using the switch statement
@@ -59,6 +52,7 @@ function displayAllProducts(categoryDropdown, table) {
                     //create a link for the name to send the user to another page to display more details of that product
                     case "productName":
                         let anchor = document.createElement("a");
+                        //is the "?" that tells the browser this does magic
                         anchor.href = `productdetails.html?productid=${aProduct["productId"]}`;
                         anchor.text = aProduct[info];
                         anchor.target = "_blank";
@@ -76,14 +70,30 @@ function displayAllProducts(categoryDropdown, table) {
     });
 
 }
+function categoryDropdownOnChange() {
+    const image = document.getElementById("categoryPicture");
+    table.style.display = "block";
+    let rows = document.querySelectorAll("tbody tr");
+    Array.from(rows).forEach(row => tableBody.removeChild(row));
+
+    fetch("http://localhost:8081/api/categories").then(response => response.json()).then(categoriesArray => {
+        // const findCategoryId = categoriesArray.find(category => category.name.toLowerCase() === categoryDropdown.value); //find the object to get it's category Id
+        const id = categoriesArray.find(category => category["name"].toLowerCase() === categoryDropdown.value).categoryId;
+        image.src = `./images/${forImagesArray[id - 1]}.jpg`;
+        image.style.display = "block";
+        displayProductPerCategory(id);
+    })
+
+
+}
 //this function is being passed each element of the category array from the first fetch call to make the second fetch call 
-function displayProductPerCategory(category) {
+function displayProductPerCategory(id) {
 
     //each category has categoryId, use that to make the GET request and get products by category, display
     //the returned data is another array but products only
 
-    fetch(`http://localhost:8081/api/categories/${category.categoryId}`).then(response => response.json()).then(products => {
-        let productsArray = products.sort( (x, y) => x.productName < y.productName ? -1 : x.productName === y.productName ? 0: 1);
+    fetch(`http://localhost:8081/api/categories/${id}`).then(response => response.json()).then(products => {
+        let productsArray = products.sort((x, y) => x.productName < y.productName ? -1 : x.productName === y.productName ? 0 : 1);
         for (let aProduct of productsArray) {
             let row = tableBody.insertRow(-1);
             //each object(element) of the array, loop through the properties and display only 3 by using the switch statement
@@ -107,6 +117,4 @@ function displayProductPerCategory(category) {
             }
         }
     });
-
-
 }
